c59356976646b1827981ef3449071f7052c20473
superpig <940578704@qq.com>
2019-08-25 17:40:37 +0800

    [Feat] webview 增加进度条

--- learn/GTDetailViewController.m
+++ learn/GTDetailViewController.m
@@ -12,10 +12,15 @@
 @interface GTDetailViewController () <WKNavigationDelegate>
 
 @property(nonatomic, strong, readwrite) WKWebView *webView;
+@property(nonatomic, strong, readwrite) UIProgressView *progressView;
 
 @end
 
 @implementation GTDetailViewController
+
+- (void)dealloc {
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,8 +28,14 @@
         self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, self.view.frame.size.height - 88)];
         self.webView;
     })];
+    [self.view addSubview:({
+        self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, 20)];
+        self.progressView;
+    })];
     self.webView.navigationDelegate = self;
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://time.geekbang.org"]]];
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
 }
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
@@ -33,6 +44,10 @@
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
     NSLog(@"didFinish");
+}
+
+- (void)observeValueForKeyPath:(nullable NSString *)keyPath ofObject:(nullable id)object change:(nullable NSDictionary<NSKeyValueChangeKey, id> *)change context:(nullable void *)context {
+    self.progressView.progress = self.webView.estimatedProgress;
 }
 
 @end
