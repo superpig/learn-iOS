294a837c59c5c24c27ae513b984cb58a0a675f96
superpig <940578704@qq.com>
2019-09-22 12:59:17 +0800

    feat(loader): nSURLSession 初次使用'

--- learn/Loader/GTListLoader.m
+++ learn/Loader/GTListLoader.m
@@ -11,12 +11,16 @@
 @implementation GTListLoader
 
 - (void)loadListData {
-    NSString *urlString = @"https://km.sankuai.com/api/collection/collection?collectionType=0&contentTypes=1";
+    NSString *urlString = @"http://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
     NSURL *listURL = [NSURL URLWithString:urlString];
     
     __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
     
-    NSLog(@"");
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"");
+    }];
+    [dataTask resume];
 }
 
 @end
